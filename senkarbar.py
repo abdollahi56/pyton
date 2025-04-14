2c2bc2b3af7beb6ce233e21e767e2eb83c09f049
abdollahi56 <a27937311@gmail.com>
2025-04-14 21:07:42 +0330

    Add files via upload

---
+++ senkarbar.py
@@ -0,0 +1,11 @@
+n=int(input("Enter age:"))
+if 0<n and n<6:
+     print("khordsal")
+elif n==6 or n<10:
+     print("koodak")
+elif n==10 or n<24:
+     print("javan")
+elif n==24 or n<40:
+     print("bozorgsal")
+elif n==40 or n<40:
+    print("miyansal")
